0107b79ec443f281be7304f9155cd607587bae82
Lorenz Gil Suico <lorenzsuico@gmail.com>
2026-09-01 10:16:41 +0800

    Frontend and Backend Setup Initialization

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+var cors = require(`cors`);
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
+
+//create express app
+const app = express();
+app.use(cors());
+
+//setup server port
+const port = process.env.PORT || 3000;
+
+//parse requrests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//parse requrests of content-type - application/json
+app.use(bodyParser.json());
+
+//define a root route
+app.get(`/`, (req, res) => {
+    res.send("Backend Development");
+});
+
+//listen for requests
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
+
+
